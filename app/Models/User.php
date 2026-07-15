8f6bf6121e69395d50578f704bc5de4ada88b473
thedev_It <bitemogilles@gmail.com>
2026-07-15 02:35:37 +0100

    Fix  SavingsGoalController name

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Models\Savingsgoal;
 
 class User extends Authenticatable
 {
@@ -40,7 +41,7 @@ class User extends Authenticatable
 
 public function savingsgoals()
 {
-    return $this->hasMany(Savingsgoal::class);
+    return $this->hasMany(SavingsGoal::class);
 }
 
     /**
