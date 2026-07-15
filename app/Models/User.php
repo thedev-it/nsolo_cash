a955415a0037e37b9fb809f872bf761c89f907d7
thedev-it <bitemogilles@gmail.com>
2026-07-15 11:40:14 +0100

    Fix import statement for SavingsGoal model

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,7 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use App\Models\Savingsgoal;
+use App\Models\SavingsGoal;
 
 class User extends Authenticatable
 {
